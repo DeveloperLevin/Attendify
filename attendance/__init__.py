cf8361765cfdbea2c2653a007f79cad80b11ce24
DeveloperLevin <21f26.levin@sjec.ac.in>
2024-07-02 23:17:54 +0530

    added login mangaer

--- attendance/__init__.py
+++ attendance/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12345"
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'admins.login'
+login_manager.login_message_category = 'info'
 
 from attendance.admins.routes import admins
 from attendance.main.routes import main

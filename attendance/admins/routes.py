da7e47451ee5f7d38ec8407a14628b530175caf4
DeveloperLevin <21f26.levin@sjec.ac.in>
2024-07-02 23:17:33 +0530

    added forms and updated templates

--- attendance/admins/routes.py
+++ attendance/admins/routes.py
@@ -1,21 +1,29 @@
 from flask import Blueprint, url_for, render_template
+from attendance.admins.forms import RegistrationForm, LoginForm, UpdateForm, DeleteForm, CreateForm
 
 admins = Blueprint('admins', __name__)
 
 @admins.route("/home")
+@login_required
 def admin_home():
     return render_template('home.html', title="Admin Home")
 
 @admins.route("/create")
+@login_required
 def create():
-    return render_template('create.html', title='Add Student')
+    form = CreateForm()
+    return render_template('create.html', title='Add Student', form=form)
 
 @admins.route("/delete")
+@login_required
 def delete():
-    return render_template('delete.html', title='Add Student')
+    form = DeleteForm()
+    return render_template('delete.html', title='Add Student', form=form)
 
 @admins.route("/update")
+@login_required
 def update():
-    return render_template('update.html', title='Add Student')
+    form = UpdateForm()
+    return render_template('update.html', title='Add Student', form=form)
 
 
